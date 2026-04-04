33b01e7ae36a05b223587869f10ec7fe254bf2b8
Atharva <manaleatharva18@gmail.com>
2026-04-04 14:50:09 +0530

    optimizatons : ratings and recommendations system

--- Backend/config.py
+++ Backend/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv("Key")
 
-    uri = os.getenv("DATABASE_URL_2")
+    uri = os.getenv("DATABASE_URL")
     if uri and uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
     SQLALCHEMY_DATABASE_URI = uri
